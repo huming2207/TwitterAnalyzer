dff0a1c0d0c3d9c12b03e500c61bada7c96cf4ca
Jackson Ming Hu <huming2207@gmail.com>
2016-02-29 23:35:18 +1100

    It works! But the code is too ugly!

--- TwitterAnalyzer/Program.cs
+++ TwitterAnalyzer/Program.cs
@@ -4,16 +4,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ini.Net;
+using System.IO;
 
 
 namespace TwitterAnalyzer
 {
     class Program
     {
+        private static string termFreq = string.Empty;
+        private static string resultCsv = string.Empty;
+        private static string confData = string.Empty;
         static void Main(string[] args)
         {
-     
-            TwitterStreamControl.StartStream(Application.StartupPath + "\term-freq.txt", Application.StartupPath + "\result.csv");
+            for(int i =0;i < args.Length;i++)
+            {
+                if(args[i].Contains("--settings"))
+                {
+                    confData = args[i + 1];
+                }
+                else if(args[i].Contains("--termfreq"))
+                {
+                    termFreq = args[i + 1];
+                }
+                else if(args[i].Contains("--result"))
+                {
+                    resultCsv = args[i + 1];
+                }
+                else if(args[i].Contains("--help") || args.Contains("-?") || args.Contains("-h"))
+                {
+                    Console.WriteLine("Twitter Analyzer");
+                    Console.WriteLine("By Jackson Ming Hu @ RMIT University, Australia");
+                    Console.WriteLine();
+                    Console.WriteLine("Usage:");
+                    Console.WriteLine("ta.exe  --settings PATH_TO_CONFIG --termfreq PATH_TO_TERMFREQ_CONFIG --result PATH_TO_RESULT_CSV");
+                }
+            }
+
+            if(File.Exists(confData) == false)
+            {
+                File.CreateText(confData);
+            }
+
+            if(File.ReadAllText(confData) != "")
+            {
+                var iniFile = new IniFile(confData);
+                TwitterStreamControl.Login(
+                    iniFile.ReadString("UserInfo", "UserKey"),
+                    iniFile.ReadString("UserInfo", "UserSecret"),
+                    iniFile.ReadString("UserInfo", "AccessToken"),
+                    iniFile.ReadString("UserInfo", "TokenKey"));
+
+                TwitterStreamControl.StartStream(termFreq, resultCsv);
+            }
+            else
+            {
+                var iniFile = new IniFile(confData);
+                iniFile.WriteString("UserInfo", "UserKey","");
+                iniFile.WriteString("UserInfo", "UserSecret", "");
+                iniFile.WriteString("UserInfo", "AccessToken", "");
+                iniFile.WriteString("UserInfo", "TokenKey", "");
+                Console.WriteLine("You have an empty setting file. Analysis will not run.");
+                
+            }
+
+
+
+            Console.CancelKeyPress += delegate(object sender, ConsoleCancelEventArgs e)
+            {
+                e.Cancel = true;
+                Console.WriteLine();
+                Console.WriteLine("Terminated by user. Thanks for using TwitterAnalyzer!");
+            };
+
+            Console.ReadKey();
+
         }
     }
 }
--- TwitterAnalyzer/TwitterStreamControl.cs
+++ TwitterAnalyzer/TwitterStreamControl.cs
@@ -23,9 +23,11 @@ namespace TwitterAnalyzer
         /// <param name="usrSecret"></param>
         /// <param name="accessToken"></param>
         /// <param name="tokenSecret"></param>
+        private static ITwitterCredentials twitterCert;
         public static void Login(string usrKey, string usrSecret, string accessToken, string tokenSecret)
         {
             Auth.ApplicationCredentials = new TwitterCredentials(usrKey, usrSecret, accessToken, tokenSecret);
+            twitterCert = new TwitterCredentials(usrKey, usrSecret, accessToken, tokenSecret);
             var user = User.GetLoggedUser();
             Console.WriteLine("======CURRENT USER INFO======");
             Console.Write("Username: ");
@@ -41,10 +43,12 @@ namespace TwitterAnalyzer
         /// </summary>
         /// <param name="tfSettingPath"></param>
         /// <param name="extraSettingPath"></param>
+        
+        //private static Tweetinvi.Core.Interfaces.Streaminvi.IFilteredStream stream = Tweetinvi.Stream.CreateFilteredStream(twitterCert);
         public static void StartStream(string tfSettingPath, string resultPath)
         {
+            var stream = Tweetinvi.Stream.CreateFilteredStream(twitterCert);
             StreamWriter writer = new StreamWriter(resultPath);
-            var stream = Tweetinvi.Stream.CreateFilteredStream();
             for(int i = 0; i < File.ReadLines(tfSettingPath).Count(); i++)
             {
                 stream.AddTrack(File.ReadLines(tfSettingPath)
@@ -54,15 +58,13 @@ namespace TwitterAnalyzer
                 );
             }
 
-            // When stream found one matching 
+            // When stream found one matching...
             stream.MatchingTweetReceived += (sender, args) =>
             {
-                Console.WriteLine("Thread " + Thread.CurrentThread.ManagedThreadId.ToString() 
-                    + "has found 1 tweet meets the requirement:" + args.MatchingTracks);
+                Console.WriteLine("Found 1 tweet meets the requirement.");
 
                 /* 
                     Record these following information:
-                      - Matching Track, which kind queried word matches?
                       - Is it retweeted? True or False?
                       - Retweet count
                       - Length of this tweet
@@ -70,28 +72,68 @@ namespace TwitterAnalyzer
                       - The tweet's country the user sent the tweet 
                       - The tweet's full name of the place where the user sent the tweet
                 */
-                string resultTweet = string.Format("{0},{1},{2},{3},{4},{5},{6}",
-                    args.MatchingTracks,
-                    args.Tweet.Text,
+
+                string resultTweet = string.Empty;
+                if (args.Tweet.Place != null) // Try recording all location info.
+                {
+                    resultTweet = @"""" + args.Tweet.Text.ToString().Replace("\r\n", "") + @"""," +
+                    args.Tweet.IsRetweet.ToString() + "," + args.Tweet.RetweetCount.ToString() + "," +
+                    args.Tweet.PublishedTweetLength.ToString() + "," + args.Tweet.CreatedBy.Name.ToString() + "," +
+                    args.Tweet.Place.Country.ToString() + "," +args.Tweet.Place.FullName.ToString();
+                }
+                
+                else //...what a pity, no location information! So, just record something else available only!
+                {
+                    resultTweet = @"""" + args.Tweet.Text.ToString().Replace("\r\n", "") + @"""," +
+                    args.Tweet.IsRetweet.ToString() + "," + args.Tweet.RetweetCount.ToString() + "," +
+                    args.Tweet.PublishedTweetLength.ToString() + "," + args.Tweet.CreatedBy.Name.ToString();
+                }
+                
+                /* sultTweet = string.Format("{0},{1},{2},{3},{4},{5},{6}",
+                    args.Tweet.Text.ToString(),
                     args.Tweet.IsRetweet.ToString(),
-                    args.Tweet.RetweetCount,
-                    args.Tweet.PublishedTweetLength,
-                    args.Tweet.CreatedBy.Name,
-                    args.Tweet.Place.Country,
-                    args.Tweet.Place.FullName
-                 );
+                    args.Tweet.RetweetCount.ToString(),
+                    args.Tweet.PublishedTweetLength.ToString(),
+                    args.Tweet.CreatedBy.Name.ToString(),
+                    args.Tweet.Place.Country.ToString(),
+                    args.Tweet.Place.FullName.ToString()
+                 ); */
                     
                 writer.WriteLine(resultTweet);
+            };
+
+            stream.StreamStopped += (sender, args) =>
+            {
+                writer.Flush();
+                writer.Dispose();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("==========STOPPED==========");
+                Console.WriteLine("TwitterAnalyzer stopped.");
+                Console.WriteLine("Reason: ");
+                Console.WriteLine(args.DisconnectMessage.Reason);
+                Console.WriteLine();
+                Console.WriteLine("Exceptions: ");
+                Console.WriteLine(args.Exception.Message);
+                Console.WriteLine();
+                Console.WriteLine(args.Exception.InnerException.Message);
+                Console.WriteLine("==========STOPPED==========");
+
+            };
+
+            stream.StreamPaused += (sender, args) =>
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("==========PAUSED==========");
+                Console.WriteLine("TwitterAnalyzer paused.");
+                Console.WriteLine("==========PAUSED==========");
             };
 
             stream.StartStreamMatchingAllConditions();
             
         }
 
-        public static void StopStream()
-        {
-
-        }
 
     }
 }
